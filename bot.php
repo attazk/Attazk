90c48a99893b3c405433db4306c7ec2437b0362b
attazk <31793962+attazk@users.noreply.github.com>
2018-02-16 21:59:37 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,8 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'อะไรเหรอ' => $text
-				//'text' => $text
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
