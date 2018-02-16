0a85b634f61482055461b92781da033b95799132
attazk <31793962+attazk@users.noreply.github.com>
2018-02-16 13:06:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 // example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-01.php
 
-include ('Attazk/php/line-bot.php');
+include ('Attazk/line-bot.php');
 
 $channelSecret = '424ee51ac8bf89a421f710db26117b46';
 $access_token  = 'xdXHxzbKD7wjbiQN4HCZEOtQTjUxqSscUU83II4hShlbKB2f5AcpgXL3vS7s3rG/F0fuxIpoDeY6zFkLbAv1YIXFtIKPi69ZYvc8Fy5kPUYmKfMiP0xTxLaDOlpPkdlsBImYLiB0X/ulcyfp4f5gFQdB04t89/1O/w1cDnyilFU=';
