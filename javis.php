3cd6419c96a1a2eb95b8daa8abbb204257a09e90
attazk <31793962+attazk@users.noreply.github.com>
2018-02-18 15:28:59 +0700

    Update javis.php

--- javis.php
+++ javis.php
@@ -35,7 +35,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
+  $arrPostData['messages'][0]['text'] = "สอนฉันหน่อย ฉันไม่เข้าใจ";
 }
  
  
