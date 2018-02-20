6a81b65f67f5846216e21551ff008142a5810fd2
attazk <31793962+attazk@users.noreply.github.com>
2018-02-20 23:14:04 +0700

    Update whoshared.php

--- whoshared.php
+++ whoshared.php
@@ -4,7 +4,7 @@ try {
   // Returns a `Facebook\FacebookResponse` object
   $response = $fb->get(
     '/2061250044110174/sharedposts',
-    '{EAACEdEose0cBADJyj5ACvULsBI6tABtkZAvHN5yYDxUoQF3GxNp3cjexB1msekLcZCnhw8EcbjIY0PMReFUVWdDZByTBk518HzUEG8fns4kT64jhNEZAdE0wqKJbaotNcxv6tpTljjc0jkRqtpqB3qgtQZAyej7N53kH1l6QIe1MrZCEhnRht9rl8QY0fMMrIZD}'
+    'EAACEdEose0cBADJyj5ACvULsBI6tABtkZAvHN5yYDxUoQF3GxNp3cjexB1msekLcZCnhw8EcbjIY0PMReFUVWdDZByTBk518HzUEG8fns4kT64jhNEZAdE0wqKJbaotNcxv6tpTljjc0jkRqtpqB3qgtQZAyej7N53kH1l6QIe1MrZCEhnRht9rl8QY0fMMrIZD'
   );
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
   echo 'Graph returned an error: ' . $e->getMessage();
